9174deb4d3c7f266fba0b37d55fdcef644845971
minusmagis <33642118+minusmagis@users.noreply.github.com>
2020-06-17 21:29:12 +0200

    Major changes
    We changed the library used to actuate the steppers (the one before was extremely jittery and the steppers did not move)
    We also changed the coms protocol to be used with a character array, arduino seems to like it better this way.
    Now it works

--- Nanopto_Filter_Flipper.ino
+++ Nanopto_Filter_Flipper.ino
@@ -2,88 +2,124 @@
    If you have any questions about it please contact minusmagis@gmail.com
 */
 
-#include <Stepper.h>
+#include <TinyStepper_28BYJ_48.h>
 
-const int stepsPerRevolution = 200;   // Change this to fit the number of steps per revolution
+//
+// pin assignments, any digital pins can be used
+//
+const int LED_PIN = 13;
+const int MOTOR_IN1_PIN = 11;
+const int MOTOR_IN2_PIN = 10;
+const int MOTOR_IN3_PIN = 9;
+const int MOTOR_IN4_PIN = 8;
+
+
+const int STEPS_PER_REVOLUTION = 2048;
+
+
+
+//
+// create the stepper motor object
+//
+TinyStepper_28BYJ_48 stepper;
+
 const int leftEndstop = 3;   // Normally open (normally high)
 const int rightEndstop = 2;  // Normally open
 boolean leftState = 0;
 boolean rightState = 0;
-boolean left = 0;
-boolean right = 0;
-String Command = "";
-int counter = 0;
 
 
+const byte CommandSize = 64;                                               // Expect commands of up to 64 bytes
+char Command[CommandSize];                                                 // initialize an array of characters that will be the received command
 
-Stepper myStepper(stepsPerRevolution, 11, 10, 9, 8); //int4->d11 int3->d10 int2->d9 int1->d8
 
 void setup() {
   pinMode(leftEndstop, INPUT_PULLUP);             //Turn on the Pullup that will pull the two endstop switches up. Connect both endstops to the d2 pin and to ground
   pinMode(rightEndstop, INPUT_PULLUP);
-  myStepper.setSpeed(120);             //Set the speed of the x axis stepper to 120 rpm?? (or steps per min I am not sure)
+  stepper.connectToPins(MOTOR_IN1_PIN, MOTOR_IN2_PIN, MOTOR_IN3_PIN, MOTOR_IN4_PIN);
+  stepper.setSpeedInStepsPerSecond(500);
+  stepper.setAccelerationInStepsPerSecondPerSecond(1000);
   Serial.begin(9600);
   Serial.println("Filter fipper arduino");
 }
 
 void loop() {
-//    Serial.println("it Works");
-//    delay(500);
-  Command = "";                                           //Restart the Command string
-  Command = readCommand();                                //Read if any command is available
-  if (Command == "70636478"  ||  Command == "38636478"  || Command == "38313246")                           //If The command is Left or left turn the left filter on
-  {
-    Serial.print("Turning right (Left Filter On)...   ");
-    while (0 < 1) {
-      if (digitalRead(rightEndstop) == false) {                                // Read the value of pin 2 and if it is true (pressed) break the while loop
-        Serial.print("Left Filter On");
-        break;
-      }
-      myStepper.step(-5);                                          // Move down the z axis motor untill the endstop (pin 2 is pressed)
-    }
-    for (int i = 8; i < 12; i++)
-    {
-      digitalWrite(i, LOW);                                     // Turn off all the pins to lower power consumption, the torque is high enough to prevent the axis from involuntarily moving.
-    }
+  GcodeCommandReceive();
+  if (Command[0] == 'r' || Command[0] == 'R') {
+    MoveRight();
   }
-  else if (Command == "7667656678"  ||  Command == "4467656678"  ||  Command == "4435333446")                          // If the master asks for the "homed" status send the homed status
-  {
-    Serial.print("Turning left (Right Filter On)...   ");
-    while (0 < 1) {
-      if (digitalRead(leftEndstop) == false) {                                // Read the value of pin 2 and if it is true (pressed) break the while loop
-        Serial.print("Right Filter On");
-        break;
-      }
-      myStepper.step(5);                                          // Move down the z axis motor untill the endstop (pin 2 is pressed)
-    }
-    for (int i = 8; i < 12; i++)
+  else  if (Command[0] == 'l' || Command[0] == 'L') {
+    MoveLeft();
+  }
+  else {
+    //    Serial.println(digitalRead(rightEndstop));
+    //    Serial.println(digitalRead(leftEndstop));
+    ResetCommand();
+    DisableSteppers();
+  }
+}
+
+void MoveRight() {
+  stepper.setCurrentPositionInSteps(0);
+  stepper.setupMoveInSteps(-2048);
+  while (!stepper.motionComplete()) {
+    stepper.processMovement();
+    if (digitalRead(rightEndstop) == 0)
     {
-      digitalWrite(i, LOW);                                     // Turn off all the pins to lower power consumption, the torque is high enough to prevent the axis from involuntarily moving.
+      break;
+    }
+    //    Serial.println("Moving Right");
+  }
+  Serial.println("Moved");
+  ResetCommand();
+}
+
+void MoveLeft() {
+  stepper.setCurrentPositionInSteps(0);
+  stepper.setupMoveInSteps(2048);
+  while (!stepper.motionComplete()) {
+    stepper.processMovement();
+    if (digitalRead(leftEndstop) == 0)
+    {
+      break;
+    }
+    //    Serial.println("Moving Left");
+  }
+  Serial.println("Moved");
+  ResetCommand();
+}
+
+void ResetCommand() {
+  for (int i = 0; i < CommandSize; i++) {
+    Command[i] = '\0';
+  }
+}
+void GcodeCommandReceive() {
+  byte ndx = 0;                                                      // Define the index where the array will be written
+  char endMarker = '\n';                                                    // Define the endmarker that will be recognized
+  char rc;                                                                  // Received character
+
+  while (Serial.available() > 0) {                   // While there is data in the serial port and the newData is not true keep reading
+    rc = Serial.read();                                                  // Read the current byte in the buffer and store it in rc
+//    Serial.println(rc);
+    if (rc != endMarker) {                                               // If rc is not equal to the endmarker it means it is part of the command so write it on the array (in the place indicated by indx)
+      Command[ndx] = rc;
+      ndx++;                                                             // Increment the ndx counter so that the next character is written in the next spot on the array
+      if (ndx >= CommandSize) {                                          // If the ndx overflows (is bigger than the commandSize specified) bring it down by one and start writing over the last value
+        ndx = CommandSize - 1;                                           // of the array thus only replacing the last char and not causing errors
+      }
+    }
+    else {
+      Command[ndx] = '\0';                                               // Else (if rc == endMarker) then write \0 null value to the end of the array
+      ndx = 0;                                                           // Reset the index counter for the next command
     }
   }
 }
 
-String readCommand() {
-  if (Serial.available() > 0)
-  {
-    String Command = "";                                          // Reset Coommand to a blank space before reading
-    delay(200);                                                    // Small delay to allow for the serial to buffer all the data
-    while(Serial.available() > 0)                  // Take the amount of digits in the serial and read them; i.e if the Serial.available() is 3 it will read the serial 3 times
-    {
-      delay(10);
-      int v = Serial.read() - 38;
-      Serial.println(v);
-      Command += (v);                       // Append the read value (-48 which is the 0 value) to the previous one to form a complete string with all the values from the serial with their real value
-    }
-    Serial.println(Command); //For debugging purposes
-    return Command;
-  }
-  else{
-    counter ++;
-    delay(50);
-    if (counter >= 100){
-      counter = 0;
-    Serial.println("...");
-    }
-  }
+void DisableSteppers() {
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
 }
+
