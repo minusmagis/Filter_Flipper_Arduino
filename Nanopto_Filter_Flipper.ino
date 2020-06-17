2b82dd6e16e89ef7b5e5def6800fcf014497ab48
minusmagis <33642118+minusmagis@users.noreply.github.com>
2020-06-17 11:20:53 +0200

    Metstable state
    Right now it works but if i change anything the comunication stops at random points for some reason

--- Nanopto_Filter_Flipper.ino
+++ Nanopto_Filter_Flipper.ino
@@ -12,6 +12,7 @@ boolean rightState = 0;
 boolean left = 0;
 boolean right = 0;
 String Command = "";
+int counter = 0;
 
 
 
@@ -30,7 +31,7 @@ void loop() {
 //    delay(500);
   Command = "";                                           //Restart the Command string
   Command = readCommand();                                //Read if any command is available
-  if (Command == "60535468"  ||  Command == "28535468"  || Command == "28212236")                           //If The command is Left or left turn the left filter on
+  if (Command == "70636478"  ||  Command == "38636478"  || Command == "38313246")                           //If The command is Left or left turn the left filter on
   {
     Serial.print("Turning right (Left Filter On)...   ");
     while (0 < 1) {
@@ -45,7 +46,7 @@ void loop() {
       digitalWrite(i, LOW);                                     // Turn off all the pins to lower power consumption, the torque is high enough to prevent the axis from involuntarily moving.
     }
   }
-  else if (Command == "6657555668"  ||  Command == "3457555668"  ||  Command == "3425232436")                          // If the master asks for the "homed" status send the homed status
+  else if (Command == "7667656678"  ||  Command == "4467656678"  ||  Command == "4435333446")                          // If the master asks for the "homed" status send the homed status
   {
     Serial.print("Turning left (Right Filter On)...   ");
     while (0 < 1) {
@@ -66,19 +67,23 @@ String readCommand() {
   if (Serial.available() > 0)
   {
     String Command = "";                                          // Reset Coommand to a blank space before reading
-    delay(100);                                                    // Small delay to allow for the serial to buffer all the data
+    delay(200);                                                    // Small delay to allow for the serial to buffer all the data
     while(Serial.available() > 0)                  // Take the amount of digits in the serial and read them; i.e if the Serial.available() is 3 it will read the serial 3 times
     {
-      Serial.println("PUTA BIDA");
-      Serial.println(Serial.available());
+      delay(10);
       int v = Serial.read() - 38;
+      Serial.println(v);
       Command += (v);                       // Append the read value (-48 which is the 0 value) to the previous one to form a complete string with all the values from the serial with their real value
     }
     Serial.println(Command); //For debugging purposes
     return Command;
   }
   else{
-    delay(500);
+    counter ++;
+    delay(50);
+    if (counter >= 100){
+      counter = 0;
     Serial.println("...");
+    }
   }
 }
